46432e5c995a6a63c64677f253fbe6db1a59457f
jay <davisjayalan@gmail.com>
2026-04-19 02:34:23 +0100

    fix: update proxy to redirect / -> /dashboard

--- src/proxy.ts
+++ src/proxy.ts
@@ -2,10 +2,9 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
  
 export function proxy(request: NextRequest) {
-    console.log(`uwu`)
-  return NextResponse.redirect(new URL('/home', request.url))
+  return NextResponse.redirect(new URL('/dashboard', request.url))
 }
  
 export const config = {
-  matcher: '/about/:path*',
+  matcher: '/',
 }
